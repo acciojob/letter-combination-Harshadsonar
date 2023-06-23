fdab650d1cc43aad7942910d896717d3461415af
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-06-23 19:34:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-}
+function letterCombinations(digits) {
 
-module.exports = letterCombinations;
+    const digitToLetters = {
+        '0': '0',
+        '1': '1',
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    };
+
+    function generateCombinations(currentIndex, currentCombination, combinations) {
+        if (currentCombination.length === digits.length) {
+            combinations.push(currentCombination);
+            return;
+        }
+
+        const letters = digitToLetters[digits[currentIndex]];
+
+        for (let i = 0; i < letters.length; i++) {
+            const letter = letters[i];
+
+            generateCombinations(currentIndex + 1, currentCombination + letter, combinations);
+        }
+    }
+
+    const combinations = [];
+    if (digits.length > 0) {
+        generateCombinations(0, '', combinations);
+    }
+    return combinations.sort();
+}
